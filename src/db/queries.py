6b22307491fbbf2befba3fee1c28454fdecf1bfe
Ruslan <kornerus@fjfi.cvut.cz>
2022-03-27 05:56:40 +0300

    create session in every query function, return message in queries

--- src/db/queries.py
+++ src/db/queries.py
@@ -8,11 +8,10 @@ from db.models import Session, BlackList, DatingUser, User
 vk = VkApi(token=GROUP_TOKEN)
 longpoll = VkLongPoll(vk)
 
-session = Session()
-
 
 def save_user(vk_id: int) -> bool:
     """ Регистрация пользователя """
+    session = Session()
     try:
         new_user = User(vk_id=vk_id)
         session.add(new_user)
@@ -22,27 +21,35 @@ def save_user(vk_id: int) -> bool:
     return True
 
 
-def delete_from_blacklist(vk_id: int) -> None:
+def delete_from_blacklist(vk_id: int, user_id: int) -> None:
     """ Удаляет пользователя из черного списка """
-    current_user = session.query(BlackList).filter_by(vk_id=vk_id).first()
+    session = Session()
+    current_user = session.query(BlackList).filter_by(
+        vk_id=vk_id, user_id=user_id).first()
     session.delete(current_user)
     session.commit()
+    return f"Пользователь {vk_id} успешно удален из Черного Списка"
 
 
-def delete_from_favorites(vk_id: int) -> None:
+def delete_from_favorites(vk_id: int, user_id: int) -> None:
     """ Удаляет пользователя из избранного """
-    current_user = session.query(DatingUser).filter_by(vk_id=vk_id).first()
+    session = Session()
+    current_user = session.query(DatingUser).filter_by(
+        vk_id=vk_id, user_id=user_id).first()
     session.delete(current_user)
     session.commit()
+    return f"Пользователь {vk_id} успешно удален из Избранных"
 
 
 def get_user(vk_id: int) -> User:
     """ получаем пользователя по `vk_id` """
+    session = Session()
     return session.query(User).filter_by(vk_id=vk_id).first()
 
 
 def check_pair_already_exists(vk_id: int) -> bool:
     """ проверят есть ли юзер в бд """
+    session = Session()
     dating_user = session.query(DatingUser).filter_by(
         vk_id=vk_id).first()
     blocked_user = session.query(BlackList).filter_by(
@@ -52,41 +59,45 @@ def check_pair_already_exists(vk_id: int) -> bool:
 
 def get_users_from_black_list(vk_id: int) -> list[BlackList]:
     """ Сбор пользователей из `черного списка` """
+    session = Session()
     user = session.query(User).filter_by(vk_id=vk_id).first()
-    return session.query(BlackList).filter_by(id_user=user.id).all()
+    return session.query(BlackList).filter_by(user_id=user.id).all()
 
 
 def get_users_from_favorites(vk_id: int) -> list[DatingUser]:
     """ Сбор пользователей из `избранных` """
+    session = Session()
     user = session.query(User).filter_by(vk_id=vk_id).first()
-    return session.query(DatingUser).filter_by(id_user=user.id).all()
+    return session.query(DatingUser).filter_by(user_id=user.id).all()
 
 
 def add_pair_to_favorites(
-    vk_id: int, id_user: int
+    vk_id: int, user_id: int
 ) -> bool:
     """ Сохранение выбранного пользователя в БД """
+    session = Session()
     try:
         new_user = DatingUser(
-            vk_id=vk_id, id_user=id_user
+            vk_id=vk_id, user_id=user_id
         )
         session.add(new_user)
         session.commit()
-        return 'ПОЛЬЗОВАТЕЛЬ УСПЕШНО ДОБАВЛЕН В ИЗБРАННОЕ'
-    except (IntegrityError, InvalidRequestError):
-        return 'Пользователь уже в избранном.'
+        return f'Пользователь {vk_id} успешно добавлен в избранное'
+    except (IntegrityError, InvalidRequestError) as e:
+        return f'Пользователь {vk_id} уже в избранном.\n {e}'
 
 
 def add_pair_to_blacklist(
-    vk_id: int, id_user: int
+    vk_id: int, user_id: int
 ) -> bool:
     """ Добавление пользователя в черный список """
+    session = Session()
     try:
         new_user = BlackList(
-            vk_id=vk_id, id_user=id_user
+            vk_id=vk_id, user_id=user_id
         )
         session.add(new_user)
         session.commit()
-        return 'Пользователь успешно заблокирован.'
+        return f'Пользователь {vk_id} успешно заблокирован.'
     except (IntegrityError, InvalidRequestError):
-        return 'Пользователь уже в черном списке.'
+        return f'Пользователь {vk_id} уже в черном списке.'
