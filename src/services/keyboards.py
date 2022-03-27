a94875cdf38c6de1e453d089e6f6102863571cfd
Ruslan <kornerus@fjfi.cvut.cz>
2022-03-27 05:56:58 +0300

    debug: return keyboard

--- src/services/keyboards.py
+++ src/services/keyboards.py
@@ -17,9 +17,11 @@ def get_menu_keyboard() -> VkKeyboard:
 def get_remove_keyboard(table: str, vk_id: int) -> VkKeyboard:
     kb = VkKeyboard(one_time=False, inline=True)
     kb.add_button(f"Remove from {table}: {vk_id}", color=VkKeyboardColor.NEGATIVE)
+    return kb
 
 
 def get_user_rate_keyboard(vk_id: int) -> VkKeyboard:
     kb = VkKeyboard(one_time=False, inline=True)
     kb.add_button(f"Add to Favorites: {vk_id}", color=VkKeyboardColor.POSITIVE)
     kb.add_button(f"Add to BlackList: {vk_id}", color=VkKeyboardColor.POSITIVE)
+    return kb
