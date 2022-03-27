b6aaed4b3361524ce59f59a88704a164850105d1
Ruslan <kornerus@fjfi.cvut.cz>
2022-03-27 09:32:38 +0300

    debug sorting photo

--- src/services/utils.py
+++ src/services/utils.py
@@ -1,6 +1,3 @@
-import datetime
-import json
-
 from db.tuple_models import Photo
 
 
@@ -10,23 +7,4 @@ def sort_photo(photos: list[Photo]) -> list[Photo]:
     for photo in photos:
         if photo.photo:
             result.append(photo)
-    return sorted(result, key=lambda photo: photo.popular)
-
-
-def json_create(lst):
-    """ JSON file create with result of programm """
-    today = datetime.date.today()
-    res = {}
-    res_list = []
-    for info in lst:
-        res['date'] = str(today)
-        res['first_name'] = info[0]
-        res['last_name'] = info[1]
-        res['link'] = info[2]
-        res['id'] = info[3]
-        res_list.append(res.copy())
-
-    with open("result.json", "a", encoding='UTF-8') as write_file:
-        json.dump(res_list, write_file, ensure_ascii=False)
-
-    print(f'Информация о загруженных файлах успешно записана в json файл.')
+    return sorted(result, key=lambda photo: photo.popular, reverse=True)
